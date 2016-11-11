9d9a0414d3f02b9b7395b62bc7e61d7187bc9b11
Jinzhu <wosmvp@gmail.com>
2016-11-11 16:09:44 +0800

    Enabue publish2 theme

--- publish2.go
+++ publish2.go
@@ -1,7 +1,30 @@
 package publish2
 
-import "github.com/qor/admin"
+import (
+	"github.com/qor/admin"
+	"github.com/qor/qor/resource"
+)
 
 func init() {
 	admin.RegisterViewPath("github.com/qor/publish2/views")
+}
+
+func (Version) ConfigureQorResource(res resource.Resourcer) {
+	enablePublishMode(res)
+}
+
+func (Schedule) ConfigureQorResource(res resource.Resourcer) {
+	enablePublishMode(res)
+}
+
+func (Visible) ConfigureQorResource(res resource.Resourcer) {
+	enablePublishMode(res)
+}
+
+func enablePublishMode(res resource.Resourcer) {
+	if res, ok := res.(*admin.Resource); ok {
+		if res.GetTheme("publish2") == nil {
+			res.UseTheme("publish2")
+		}
+	}
 }
