6ccce2c812dabfe2a4bf2aa638b6a33d90fd19ca
Jinzhu <wosmvp@gmail.com>
2016-11-11 17:27:05 +0800

    Add publish_versions tmpl

--- publish2.go
+++ publish2.go
@@ -4,6 +4,7 @@ import (
 	"path"
 
 	"github.com/qor/admin"
+	"github.com/qor/qor"
 	"github.com/qor/qor/resource"
 )
 
@@ -28,9 +29,20 @@ func enablePublishMode(res resource.Resourcer) {
 		if res.GetTheme("publish2") == nil {
 			res.UseTheme("publish2")
 
+			res.Meta(&admin.Meta{
+				Name:  "Versions",
+				Label: "Versions",
+				Type:  "publish_versions",
+				Valuer: func(interface{}, *qor.Context) interface{} {
+					return ""
+				},
+			})
+
 			router := res.GetAdmin().GetRouter()
 			ctr := controller{Resource: res}
 			router.Get(path.Join(res.ToParam(), res.ParamIDName(), "versions"), ctr.Versions, admin.RouteConfig{Resource: res})
+
+			res.IndexAttrs(res.IndexAttrs(), "Versions")
 		}
 	}
 }
