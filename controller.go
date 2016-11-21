5949ce4cec1fa1e1b63f770d079bce73753e1669
Jinzhu <wosmvp@gmail.com>
2016-11-21 10:58:45 +0800

    Tweak versions page

--- controller.go
+++ controller.go
@@ -2,6 +2,7 @@ package publish2
 
 import (
 	"fmt"
+	"html/template"
 
 	"github.com/qor/admin"
 )
@@ -18,6 +19,15 @@ func (ctr controller) Versions(context *admin.Context) {
 	records := context.Resource.NewSlice()
 	context.GetDB().Set(VersionMode, VersionMultipleMode).Set(ScheduleMode, ModeOff).Set(VisibleMode, ModeOff).Find(records, fmt.Sprintf("%v = ?", context.Resource.PrimaryDBName()), context.ResourceID)
 
-	result := context.Render("publish2/versions", records)
+	result := context.Funcs(template.FuncMap{
+		"version_metas": func() (metas []*admin.Meta) {
+			for _, name := range []string{"VersionName", "ScheduledStartAt", "ScheduledEndAt", "PublishReady"} {
+				if meta := ctr.Resource.GetMeta(name); meta != nil {
+					metas = append(metas, meta)
+				}
+			}
+			return
+		},
+	}).Render("publish2/versions", records)
 	context.Writer.Write([]byte(result))
 }
