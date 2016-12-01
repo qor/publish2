c11dfc8f0c60abecefd968f20594f1d76bd577fa
Jinzhu <wosmvp@gmail.com>
2016-12-01 12:21:24 +0800

    Allow clear publish scheduled time with blank string

--- utils.go
+++ utils.go
@@ -9,14 +9,12 @@ import (
 
 func GetScheduledTime(request *http.Request, writer http.ResponseWriter) string {
 	var scheduledTime string
-	if str := request.URL.Query().Get("publish_scheduled_time"); str != "" {
-		scheduledTime = str
-	}
-
-	if scheduledTime == "" {
-		if cookie, err := request.Cookie("publish_scheduled_time"); err == nil {
-			scheduledTime = cookie.Value
+	if values, ok := request.URL.Query()["publish_scheduled_time"]; ok {
+		if len(values) > 0 && values[0] != "" {
+			scheduledTime = values[0]
 		}
+	} else if cookie, err := request.Cookie("publish_scheduled_time"); err == nil {
+		scheduledTime = cookie.Value
 	}
 
 	context := qor.Context{Request: request, Writer: writer}
